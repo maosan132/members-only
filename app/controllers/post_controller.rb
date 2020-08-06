65bd6ff756fc59ea49c551387a2c0312a59f468a
Yahir Cardona <yahir.cardona@gmail.com>
2020-08-06 15:45:18 -0600

    form added to post/new

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,24 +1,25 @@
 class PostController < ApplicationController
-  before_action :require_login, only: [:index, :new, :create]
+  # before_action :require_login, only: [:index, :new, :create]
 
-  def index 
-    @post = Post.all
-  end
   
   def new
     @post = Post.new
   end
   
   def create
-    @post = Post.new(params)
-    @post.save
-    redirect_to post_path(@post)
+    @post = Post.new(post_params)
+    # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+
+    if @post.save
+      redirect_to new_post_path
+    else
+      render :new
+    end
   end
   private
 
   def post_params
-    params.require(:post).permit(:username, :email, :password)
+    params.require(:post).permit(:title, :body, :user_id)
   end
 
-  
 end
