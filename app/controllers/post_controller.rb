ef90485884be57b239576cf78b1ae5b6697c410b
maosan132 <maosan132@gmail.com>
2020-08-06 15:40:00 -0500

    configured init files

---
+++ app/controllers/post_controller.rb
@@ -0,0 +1,19 @@
+class PostController < ApplicationController
+  before_action :require_login, only: [:index, :new, :create]
+
+  def index 
+    @post = Post.all
+  end
+  
+  def new
+    @post = Post.new
+  end
+  
+  def create
+    @post = Post.new(params)
+    @post.save
+    redirect_to post_path(@post)
+  end
+
+  
+end
